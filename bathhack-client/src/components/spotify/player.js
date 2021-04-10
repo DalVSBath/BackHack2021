429f7fc4ccc1e16d30eb0c977da43e1328f0be32
Dalhome3 <dal34@bath.ac.uk>
2021-04-10 14:11:14 +0100

    Spotifyyyyyyyyyyyyyy

--- bathhack-client/src/components/spotify/player.js
+++ bathhack-client/src/components/spotify/player.js
@@ -1,29 +1,105 @@
-window.onSpotifyWebPlaybackSDKReady = () => {
-    const token = '[My Spotify Web API access token]';
-    const player = new Spotify.Player({
-      name: 'Web Playback SDK Quick Start Player',
-      getOAuthToken: cb => { cb(token); }
-    });
+import React from "react";
+import Script from 'react-load-script'
+class Player extends React.Component {
+    constructor(props) {
+        super(props);
 
-    // Error handling
-    player.addListener('initialization_error', ({ message }) => { console.error(message); });
-    player.addListener('authentication_error', ({ message }) => { console.error(message); });
-    player.addListener('account_error', ({ message }) => { console.error(message); });
-    player.addListener('playback_error', ({ message }) => { console.error(message); });
+        this.player = null;
 
-    // Playback status updates
-    player.addListener('player_state_changed', state => { console.log(state); });
+        this.state = {
+            device_id: null,
+            token: "BQCm5ZCts0ZwCYRk8GStIQ_ht-AQfypaCvsgKFhZmVPkBcDuRYACU5SlqQkHCKytMAmcSya_pq28lU5PYoEZ7fG6l04mIIu94Osf_KCFAlBvVvxUNa2x7aBDAKLrUK2NGvU1u9EL6VIFIbfaxUByPfJewwaXXVwH",
+        }
+    }
+    
 
-    // Ready
-    player.addListener('ready', ({ device_id }) => {
-      console.log('Ready with Device ID', device_id);
-    });
+    resume = () => {
+        this.player.togglePlay().then(() => {
+            console.log('Resumed!');
+          });
+    }
 
-    // Not Ready
-    player.addListener('not_ready', ({ device_id }) => {
-      console.log('Device ID has gone offline', device_id);
-    });
+    play = ({
+        spotify_uri,
+        id,
+        token
+      }) => {
+          fetch(`https://api.spotify.com/v1/me/player/play?device_id=${id}`, {
+            method: 'PUT',
+            body: JSON.stringify({ uris: [spotify_uri] }),
+            headers: {
+              'Content-Type': 'application/json',
+              'Authorization': `Bearer BQCm5ZCts0ZwCYRk8GStIQ_ht-AQfypaCvsgKFhZmVPkBcDuRYACU5SlqQkHCKytMAmcSya_pq28lU5PYoEZ7fG6l04mIIu94Osf_KCFAlBvVvxUNa2x7aBDAKLrUK2NGvU1u9EL6VIFIbfaxUByPfJewwaXXVwH`
+            },
+          });
+      };
+    handleScriptLoad = () => {
+        return new Promise(resolve => {
+          if (window.Spotify) {
+            resolve();
+          } else {
+            window.onSpotifyWebPlaybackSDKReady = resolve;
+          }
+        }).then(() => {
+            const token = 'BQCm5ZCts0ZwCYRk8GStIQ_ht-AQfypaCvsgKFhZmVPkBcDuRYACU5SlqQkHCKytMAmcSya_pq28lU5PYoEZ7fG6l04mIIu94Osf_KCFAlBvVvxUNa2x7aBDAKLrUK2NGvU1u9EL6VIFIbfaxUByPfJewwaXXVwH';
+            this.player = new window.Spotify.Player({
+                name: 'Web Playback SDK Quick Start Player',
+                getOAuthToken: cb => { cb(token); }
+            });
 
-    // Connect to the player!
-    player.connect();
-  };
+        // Error handling
+        this.player.addListener('initialization_error', ({ message }) => { console.error(message); });
+        this.player.addListener('authentication_error', ({ message }) => { console.error(message); });
+        this.player.addListener('account_error', ({ message }) => { console.error(message); });
+        this.player.addListener('playback_error', ({ message }) => { console.error(message); });
+
+            // Playback status updates
+            this.player.addListener('player_state_changed', state => { console.log(state); });
+
+            // Ready
+            this.player.addListener('ready', ({ device_id }) => {
+                console.log('Ready with Device ID', device_id);
+
+                this.setState({device_id: device_id});
+            });
+
+            // Not Ready
+            this.player.addListener('not_ready', ({ device_id }) => {
+                console.log('Device ID has gone offline', device_id);
+            });
+
+            var time = setTimeout(() => {
+                this.play({
+                id: this.state.device_id,
+                token: "BQCm5ZCts0ZwCYRk8GStIQ_ht-AQfypaCvsgKFhZmVPkBcDuRYACU5SlqQkHCKytMAmcSya_pq28lU5PYoEZ7fG6l04mIIu94Osf_KCFAlBvVvxUNa2x7aBDAKLrUK2NGvU1u9EL6VIFIbfaxUByPfJewwaXXVwH",
+                spotify_uri: 'spotify:track:7xGfFoTpQ2E7fRF5lN10tr',
+                });
+                clearTimeout(time);
+            }, 2500);
+
+            var time2 = setTimeout(() => {
+                this.resume();
+                clearTimeout(time2);
+            }, 5000)
+
+            // Connect to the player!
+            this.player.connect();
+            });
+      }
+
+    handleScriptError = e => {
+        console.error(e);
+    }
+
+    render() {
+        return (
+            <Script 
+                url="https://sdk.scdn.co/spotify-player.js" 
+                onError={this.handleScriptError} 
+                onLoad={this.handleScriptLoad}
+            />
+        );
+    }
+}
+
+export default Player;
--- bathhack-client/src/pages/clients/viber.js
+++ bathhack-client/src/pages/clients/viber.js
@@ -1,8 +1,13 @@
 import React from 'react';
+import Player from '../../components/spotify/player'
+
 
 const Viber = () => {
     return (
+        <>
+        <Player />
         <p>this is the viber screen (Thanks for the amazing name Dan)</p>
+        </>
     )
 }
 
