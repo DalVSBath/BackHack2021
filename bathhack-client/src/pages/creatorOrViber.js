5faceb3386838e2381e11557782a0d45b2ccd0dc
LucaVolentir <luca.volentir@gmail.com>
2021-04-10 20:42:37 +0100

    gui looks amazing

--- bathhack-client/src/pages/creatorOrViber.js
+++ bathhack-client/src/pages/creatorOrViber.js
@@ -12,7 +12,7 @@ class creatorOrViber extends React.Component {
         window.open("http://localhost:3000/viber");
     }
     render() {
-        return (<div style={{display: "flex", alignItems: "center", justifyContent: "center", height:"100vh"}}>
+        return (<div style={{display: "flex", alignItems: "center", justifyContent: "center", height:"100vh", width:"100wh"}}>
                 <button className='e-link' onClick={this.btnClickCreator.bind(this)}>CREATOR</button>
                 <button className="e-link" onClick={this.btnClickViber.bind(this)}>VIBER</button>
             </div>);
