23123fb6c71faba0ef83bf2317edfd0a2d820474
zachary upstone <zu213@bath.ac.uk>
2021-04-10 21:58:20 +0100

    urmum

--- bathhack-client/src/components/ddr/score.js
+++ bathhack-client/src/components/ddr/score.js
@@ -9,14 +9,20 @@ class Score extends Component {
         }
     }
 
-    increment = () =>{
+    smallIncrement = () =>{
         this.setState( {count: this.state.count + 1 });
     }
+    medIncrement = () =>{
+      this.setState( {count: this.state.count + 2 });
+    }
+    bigIncrement = () =>{
+    this.setState( {count: this.state.count + 3 });
+  }
 
   render() {
     return (
       <div className style={{"position":"absolute","right":"30px","top":"2vh"}}> 
-            <h2>{this.state.count}</h2>
+            <h2 style={{"color":"green"}}>{this.state.count}</h2>
      </div>
     );
   }
