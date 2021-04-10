e815dd4fd60c3a6f589e381378804c1778f00273
Dalhome3 <dal34@bath.ac.uk>
2021-04-10 16:52:14 +0100

    small changes

--- bathhack-client/src/components/sockets/getSockets.js
+++ bathhack-client/src/components/sockets/getSockets.js
@@ -1,14 +1,14 @@
 import { w3cwebsocket } from "websocket";
 
 const getCreatorSocket = () => {
-    const client = new w3cwebsocket('ws://127.0.0.1:8080');
+    const client = new w3cwebsocket('ws://127.0.0.1:4180');
     client.onopen = () => {
         console.log("Creator socket bound ");
     }
 }
 
 const getViberSocket = () => {
-    const client = new w3cwebsocket('ws://127.0.0.1:8080');
+    const client = new w3cwebsocket('ws://127.0.0.1:4180');
     client.onopen = () => {
         console.log("Viber socket bound ");
     }
