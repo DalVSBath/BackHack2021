5698b0002433cf33d279dae42ebe8ea32cbb8b44
LucaVolentir <luca.volentir@gmail.com>
2021-04-10 19:21:45 +0100

    i am not sure about the css

---
+++ bathhack-client/src/components/ddr/creatorOrViber.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import 'creatorOrViber.css'
+
+
+class App extends React.Component {
+    // Click Event.
+    btnClickCreator() {
+        window.open("http://localhost:3000/viber");
+    }
+
+    btnClickViber() {
+        window.open("http://localhost:3000/viber");
+    }
+    render() {
+        return (<div>
+                <ButtonComponent cssClass='e-link' onClick={this.btnClickCreator.bind(this)}>CREATOR</ButtonComponent>
+                <ButtonComponent cssClass="e-link" onClick={this.btnClickViber.bind(this)}>VIBER</ButtonComponent>
+            </div>);
+    }
+}
