24a9bd9ea835dd589021ba90fe9d0f1f47f0d242
Dalhome3 <dal34@bath.ac.uk>
2021-04-10 16:04:37 +0100

    Musics

--- bathhack-client/src/components/spotify/player.js
+++ bathhack-client/src/components/spotify/player.js
@@ -9,11 +9,28 @@ class Player extends React.Component {
 
         this.state = {
             device_id: null,
-            token: "BQCm5ZCts0ZwCYRk8GStIQ_ht-AQfypaCvsgKFhZmVPkBcDuRYACU5SlqQkHCKytMAmcSya_pq28lU5PYoEZ7fG6l04mIIu94Osf_KCFAlBvVvxUNa2x7aBDAKLrUK2NGvU1u9EL6VIFIbfaxUByPfJewwaXXVwH",
+            token: "BQAhwloNRiDgPnF3NE_y9W6WNuWaReoUGZUWwafb7nvUlpa6-kq2-Lfqaoyf4mc1GMV-8DgjZYJgO8PWmPl-V4RvrILjAQv-3ZP4QFBAStDhEybgly4zhTTg61dB6Q2IcL2SDmYf2ZKRw0Pd3aQB-NDyU4z9cMqP",
         }
     }
-    
     static contextType = SocketContext;
+
+    updateTime = time => {
+        this.player.seek(time * 1000).then(() => {
+            console.log('Changed position!');
+          });
+    }
+
+    componentDidUpdate(prevProps) {
+        if(this.props.playing !== prevProps.playing) {
+            this.togglePlayback();
+        }
+    }
+
+    togglePlayback = () => {
+        this.player.togglePlay().then(() => {
+            console.log(`Now playing: ${this.props.playing}`);
+          });
+    }
 
     resume = () => {
         this.player.resume().then(() => {
@@ -22,6 +39,12 @@ class Player extends React.Component {
                 let volume_percentage = volume * 100;
                 console.log(`The volume of the player is ${volume_percentage}%`);
               });
+          });
+    }
+
+    pause = () => {
+        this.player.pause().then(() => {
+            console.log('Paused!');
           });
     }
 
@@ -36,6 +59,13 @@ class Player extends React.Component {
               'Content-Type': 'application/json',
               'Authorization': `Bearer ${this.state.token}`
             },
+          }).then(() => {
+              if(this.props.playing) {
+                  this.resume();
+              }else{
+                  this.pause();
+              }
+              this.props.ready();
           });
       };
     handleScriptLoad = () => {
@@ -48,7 +78,7 @@ class Player extends React.Component {
         }).then(() => {
             const token = this.state.token;
             this.player = new window.Spotify.Player({
-                name: 'Web Playback SDK Quick Start Player',
+                name: 'Beat Breaker',
                 getOAuthToken: cb => { cb(token); }
             });
 
@@ -59,32 +89,23 @@ class Player extends React.Component {
         this.player.addListener('playback_error', ({ message }) => { console.error(message); });
 
             // Playback status updates
-        this.player.addListener('player_state_changed', state => { console.log(state); });
+        //this.player.addListener('player_state_changed', state => { console.log(state); });
 
         // Ready
         this.player.addListener('ready', ({ device_id }) => {
             console.log('Ready with Device ID', device_id);
 
             this.setState({device_id: device_id});
+            this.play({
+                id: device_id,
+                spotify_uri: 'spotify:track:' + this.props.trackId,
+                });
         });
 
         // Not Ready
         this.player.addListener('not_ready', ({ device_id }) => {
             console.log('Device ID has gone offline', device_id);
         });
-
-        var time = setTimeout(() => {
-            this.play({
-            id: this.state.device_id,
-            spotify_uri: 'spotify:track:6730LysZdBvgjo5MgWo4Tm',
-            });
-            clearTimeout(time);
-        }, 2500);
-
-        var time2 = setTimeout(() => {
-            this.resume();
-            clearTimeout(time2);
-        }, 5000)
 
         // Connect to the player!
         this.player.connect();
--- bathhack-client/src/pages/clients/viber.js
+++ bathhack-client/src/pages/clients/viber.js
@@ -1,12 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Player from '../../components/spotify/player'
 
 
 const Viber = () => {  
+    const [playing, setPlaying] = useState(false);
+
     return (
         <>
-        <Player />
-        <p>this is the viber screen (Thanks for the amazing name Dan)</p>
+            <Player playing={playing} trackId="6730LysZdBvgjo5MgWo4Tm" ready={() => console.log("Ready")} />
+            <p>this is the viber screen (Thanks for the amazing name Dan)</p>
+            <button onClick={() => setPlaying(!playing)}>This Is Button</button>
         </>
     )
 }
