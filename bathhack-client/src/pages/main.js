f43d13f3b630884b6f9c45256744d5cc6d4bee8a
zachary upstone <zu213@bath.ac.uk>
2021-04-11 07:33:05 +0100

    hi

--- bathhack-client/src/pages/main.js
+++ bathhack-client/src/pages/main.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import logo from "../content/logo3.png";
 import {Link} from 'react-router-dom';
+import sound from "../content/music.mp3";
+import ReactSound from 'react-sound';
 
 const Main = () => {
 
     return (
+        <>
+            <ReactSound
+            url = {sound}
+            playStatus={ReactSound.status.PLAYING}
+            />
         <div style={{position: "absolute", display: "flex", alignItems: "center", justifyContent: "center", height:"100vh", width: "100vw"}}>
             <div style={{height:"90vh"}}> <h1 style={{fontSize: "70px",top:"10vh", width:"100%"}}>Welcome to </h1></div>
             <div style={{position: "absolute",top:"35vh",height:"35vh", alignItems:"center",justifyItems:"center"}}><img src={logo}/></div>
@@ -16,6 +23,7 @@ const Main = () => {
                 </Link>
             </div>
         </div>
+        </>
     );
 }
 
