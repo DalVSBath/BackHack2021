dd5d294a88e79d3b012260c8f47c32c693a3ba4e
Henry Allen <hca32@bath.ac.uk>
2021-04-11 07:38:14 +0100

    ui

--- bathhack-client/src/pages/main.js
+++ bathhack-client/src/pages/main.js
@@ -9,9 +9,9 @@ const Main = () => {
             <div style={{height:"90vh"}}> <h1 style={{fontSize: "70px",top:"10vh", width:"100%"}}>Welcome to </h1></div>
             <div style={{position: "absolute",top:"35vh",height:"35vh", alignItems:"center",justifyItems:"center"}}><img src={logo}/></div>
             <div style={{position: "absolute",height:"30vh",top:"60vh",}}>
-                <Link style={{fontSize: "80px"}} to={"/Spotify/Login"}>
-                    <button style={{backgroundColor: "Transparent", backgroundRepeat: "no-repeat", border: "none", cursor: "pointer", overflow: "hidden", outline: "none"}}>
-                        Click to Start
+                <Link to={"/Spotify/Login"}>
+                    <button style={{ whiteSpace: 'nowrap', fontSize: "72px", backgroundColor: "Transparent", backgroundRepeat: "no-repeat", border: "none", cursor: "pointer", overflow: "hidden", outline: "none"}}>
+                        {"> CLICK TO START <"}
                     </button>
                 </Link>
             </div>
--- bathhack-client/src/pages/spotify/login.js
+++ bathhack-client/src/pages/spotify/login.js
@@ -1,11 +1,18 @@
 import React from "react";
+import logo from "../../content/spotify-logo.svg";
 
 class Login extends React.Component {
     render() {
         return (
-            <a href={
-                `https://accounts.spotify.com/authorize?client_id=50f563707d1041d5bc9237357be49ee6&response_type=code&redirect_uri=${encodeURIComponent("http://localhost:3000/spotify/callback/")}&scope=user-read-private%20user-read-email%20streaming&show_dialog=true`
-            }>Login</a>
+            <div style={{position: "absolute", display: "flex", alignItems: "center", justifyContent: "center", flexFlow: "column", height:"100vh", width: "100vw"}}>
+                <img src={logo} alt="spotify" style={{height: "35%", paddingBottom: "20px;"}} />
+                <button style={{ whiteSpace: 'nowrap', fontSize: "72px", backgroundColor: "Transparent", backgroundRepeat: "no-repeat", border: "none", cursor: "pointer", overflow: "hidden", outline: "none"}}>
+                    <a style={{textDecoration: "none"}} href={
+                        `https://accounts.spotify.com/authorize?client_id=50f563707d1041d5bc9237357be49ee6&response_type=code&redirect_uri=${encodeURIComponent("http://localhost:3000/spotify/callback/")}&scope=user-read-private%20user-read-email%20streaming&show_dialog=true`
+                    }>Connect to Spotify</a>
+                </button>
+                
+            </div>
         );
     }
 }
